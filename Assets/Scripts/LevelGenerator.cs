aff77118ff26fd7a91b5a1847f3d36f48932f5c4
kitsunnet | Мацко Егор <76002087+fuett966@users.noreply.github.com>
2024-04-13 22:58:38 +0300

    init generator

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject sphere;
+    [SerializeField] private GameObject note;
+    [SerializeField] private GameObject tube;
+    [SerializeField] private GameObject environment;
+
+    public void GenerateLevel()
+    {
+        
+    }
+    
+}
